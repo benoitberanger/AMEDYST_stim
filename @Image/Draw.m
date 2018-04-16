7ebd8c259e8ab2ecc9a801ac0a67a5ba9f76d6de
Benoît Béranger <benoit.beranger@icm-institute.org>
2018-04-16 14:00:31 +0200

    no reward = reward + red cross over it

--- @Image/Draw.m
+++ @Image/Draw.m
@@ -8,7 +8,10 @@ switch status
     case 1
         Screen('DrawTexture', self.wPtr, self.texturePtr , [],  self.currentRect);
     case 0
-        Screen('FillOval', self.wPtr ,self.currentColor, self.currentRect);
+        % Screen('FillOval', self.wPtr ,self.currentColor, self.currentRect);
+        Screen('DrawTexture', self.wPtr, self.texturePtr , [],  self.currentRect);
+        Screen('DrawLine'   , self.wPtr , [255 0 0] , self.currentRect(1), self.currentRect(2), self.currentRect(3), self.currentRect(4) , self.baseRect(end)/100);
+        Screen('DrawLine'   , self.wPtr , [255 0 0] , self.currentRect(1), self.currentRect(4), self.currentRect(3), self.currentRect(2) , self.baseRect(end)/100);
     otherwise
         error('status must be 0 or 1')
 end % switch
